d2b1b6788fedfe9171d7615fd2dd4d1fd1a8e1f8
Aicy-nina <muhikanina@gmail.com>
2024-04-03 16:14:00 +0300

    review readme

---
+++ README.js
@@ -0,0 +1,24 @@
+### 1. Student Grade Genarator
+
+This program prompts the user to input student marks and outputs the corresponding grades based on predefined grade boundaries.
+
+-   Grade A: > 79
+-   Grade B: 60 to 79
+-   Grade C: 50 to 59
+-   Grade D: 40 to 49
+-   Grade E: Less than 40
+
+### 2. Speed Detector 
+
+This program takes the speed of a car as input and calculates demerit points for exceeding the speed limit.
+
+-   Speed Limit: 70 km/h
+-   For every 5 km/h above the speed limit, one demerit point is assigned.
+-   If the driver accumulates more than 12 demerit points, the program prints "License suspended".
+
+### 3. Net Salary Calculator
+
+This program calculates an individual’s net salary based on their basic salary, benefits, and deductions such as taxes, NHIF, and NSSF.
+
+-   Payee (Tax) calculation based on [KRA Tax Rates](https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/paye)
+-   NHIF and NSSF deductions based on [NHIF and NSSF rates](https://www.aren.co.ke/payroll/taxrates.htm)
