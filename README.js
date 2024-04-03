b38adbb7c6f411c15759d190e7d5566328ac1847
Aicy-nina <163542699+Aicy-nina@users.noreply.github.com>
2024-04-03 20:02:59 +0300

    Update README.js

--- README.js
+++ README.js
@@ -1,24 +1,43 @@
-### 1. Student Grade Genarator
+# Week 1 Code challenge
 
-This program prompts the user to input student marks and outputs the corresponding grades based on predefined grade boundaries.
+#### This involves 3 Toy Problems
 
--   Grade A: > 79
--   Grade B: 60 to 79
--   Grade C: 50 to 59
--   Grade D: 40 to 49
--   Grade E: Less than 40
+#### **By Nina Muhika**
 
-### 2. Speed Detector 
+## Project Description
 
-This program takes the speed of a car as input and calculates demerit points for exceeding the speed limit.
+#### Challenge 1: Student Grade Generator
 
--   Speed Limit: 70 km/h
--   For every 5 km/h above the speed limit, one demerit point is assigned.
--   If the driver accumulates more than 12 demerit points, the program prints "License suspended".
+    Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade:
 
-### 3. Net Salary Calculator
+    - A > 79
+    - B - 60 to 79
+    - C - 59 to 49
+    - D - 40 to 49
+    - E - less than 40.
 
-This program calculates an individual’s net salary based on their basic salary, benefits, and deductions such as taxes, NHIF, and NSSF.
+## Setup/Installation Requirements
 
--   Payee (Tax) calculation based on [KRA Tax Rates](https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/paye)
--   NHIF and NSSF deductions based on [NHIF and NSSF rates](https://www.aren.co.ke/payroll/taxrates.htm)
+    - Download a zip file in the code section to your desired folder
+    - Extract the files
+    - Open the extracted folder with vs code, you see 3 files with .js extension.
+    - Run individual files/questions in your terminal using ``node <name of your file>``
+
+## Known Bugs
+
+    The application works perfectly well, no bugs.
+
+## Technologies used
+
+    - JS
+    - Git
+    - Github
+
+## Support and contact details
+
+    - email :: ninamuhika@gmail.com
+
+### License
+
+\*Licenced under the MIT License
+Copyright (c) 2024 \*\*Nina Muhika-👋 
